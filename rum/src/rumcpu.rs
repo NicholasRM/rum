750fafa455c52afb146d25cbea5b4287d0a90e39
matt <matthew_kelley@uri.edu>
2022-12-06 16:02:14 -0500

    finished instructions for rumCPU

--- rum/src/rumcpu.rs
+++ rum/src/rumcpu.rs
@@ -8,12 +8,30 @@ impl RumCpu {
         RumCpu { pc: 0, regs: [0; 8] }
     }
 
-    pub fn cmov() {todo!()}
-    pub fn add() {todo!()}
-    pub fn mul() {todo!()}
-    pub fn div() {todo!()}
-    pub fn nand() {todo!()}
+    pub fn cmov(&mut self, reg_a: usize, reg_b: usize, reg_c: usize) {
+        if self.regs[reg_c] != 0{
+            self.regs[reg_a] = self.regs[reg_b];
+        }
+    }
+    pub fn add(&mut self, reg_a: usize, reg_b: usize, reg_c: usize) {
+        let temp_b: u64 = self.regs[reg_b] as u64;
+        let temp_c: u64 = self.regs[reg_c] as u64;
+        self.regs[reg_a] = ((temp_b + temp_c) % (u32::MAX) as u64) as u32;
+    }
+    pub fn mul(&mut self, reg_a: usize, reg_b: usize, reg_c: usize) {
+        let temp_b: u64 = self.regs[reg_b] as u64;
+        let temp_c: u64 = self.regs[reg_c] as u64;
+        self.regs[reg_a] = ((temp_b * temp_c) % (u32::MAX) as u64) as u32;
+    }
+    pub fn div(&mut self, reg_a: usize, reg_b: usize, reg_c: usize) {
+        self.regs[reg_a] = self.regs[reg_b] / self.regs[reg_c];
+    }
+    pub fn nand(&mut self, reg_a: usize, reg_b: usize, reg_c: usize) {
+        self.regs[reg_a] = !(self.regs[reg_b] & self.regs[reg_c]);
+    }
     pub fn output() {todo!()}
     pub fn input() {todo!()}
-    pub fn load_value() {todo!()}
+    pub fn load_value(&mut self, load_reg: usize, value: u32) {
+        self.regs[load_reg] = value;
+    }
 }
