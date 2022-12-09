3cfac65cff27ef80a2a93b0e7beaec846c549504
matt <matthew_kelley@uri.edu>
2022-12-09 11:17:42 -0500

    input and execute finished

--- rum/src/rumdata.rs
+++ rum/src/rumdata.rs
@@ -1,7 +1,9 @@
 use core::panic;
 
-use crate::rumcpu::RumCpu;
+use crate::ruminfoextr::{get_three_regs, get_one_reg, get_load_reg, get_value};
+use crate::{rumcpu::RumCpu, ruminfoextr::get_opcode};
 use crate::rummemory::RumMemory;
+use std::io::{Read, self};
 
 enum Opcode {
     CMov,
@@ -31,13 +33,77 @@ impl RumData {
     }
 
     pub fn execute(&mut self) {
-        todo!();
+        let mut instruction = 0;//get it from seg0
+        loop{
+            instruction = self.memory.get_seg_val(0, self.cpu.pc);
+            match get_opcode(instruction){
+                0 => {
+                    let (reg_a, reg_b, reg_c) = get_three_regs(instruction);
+                    self.cpu.cmov(reg_a, reg_b, reg_c)}
+                1 => {
+
+                }
+                2 => {
+
+                }
+                3 => {
+                    let (reg_a, reg_b, reg_c) = get_three_regs(instruction);
+                    self.cpu.add(reg_a, reg_b, reg_c)}
+                4 => {
+                    let (reg_a, reg_b, reg_c) = get_three_regs(instruction);
+                    self.cpu.mul(reg_a, reg_b, reg_c)}
+                5 => {
+                    let (reg_a, reg_b, reg_c) = get_three_regs(instruction);
+                    self.cpu.div(reg_a, reg_b, reg_c)}
+                6 => {
+                    let (reg_a, reg_b, reg_c) = get_three_regs(instruction);
+                    self.cpu.nand(reg_a, reg_b, reg_c)}
+                7 => {
+                    std::process::exit(0);
+                }
+                8 => {
+
+                }
+                9 => {
+
+                }
+                10 => {
+                    let reg_c = get_one_reg(instruction);
+                    self.output(reg_c);
+                }
+                11 => {
+                    let reg_c = get_one_reg(instruction);
+                    self.input(reg_c);
+                }
+                12 => {
+
+                }
+                13 => {
+                    let load_reg = get_load_reg(instruction);
+                    let value = get_value(instruction);
+                    self.cpu.load_value(load_reg, value);
+                }
+                _ => {
+                    panic!("invalid instruction");
+                }
+            }
+        }
     }
     
     pub fn output(&self, reg_c: usize) {
         if self.cpu.regs[reg_c] > 255{
             panic!("Value too big");
         }
+        print!("{}", self.cpu.regs[reg_c] as u8 as char);
     }
-    fn input() {todo!()}
+
+    pub fn input(&mut self, reg_c: usize) {
+        let mut buffer: [u8; 1] = [0];
+        let input = io::stdin().read(&mut buffer);
+        match input.unwrap(){
+            0 => self.cpu.load_value(reg_c, u32::MAX),
+            1 => self.cpu.load_value(reg_c, buffer[0] as u32),
+            _ => panic!("Too many bites in buffer"),
+        }
+    }
 }
--- rum/src/rumload.rs
+++ rum/src/rumload.rs
@@ -1,1 +1,9 @@
-pub fn parse_input(input: Option<&str>) -> Vec<u32> {todo!()}
+use std::io::Read;
+
+pub fn parse_input(input: Option<&str>) -> Vec<u32> {
+    let file_name = input.unwrap();
+    let mut reader = std::fs::File::open(file_name).unwrap();
+    let mut buffer: Vec<u8> = Vec::new();
+    reader.read_to_end(&mut buffer).unwrap();
+    buffer.chunks_exact(4).map(| words | u32::from_be_bytes(words.try_into().unwrap())).collect()
+}
