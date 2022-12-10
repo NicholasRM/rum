c80154a836b62ca4607be4bb1e283cc4f0271cf7
Nicholas <77742083+NicholasRM@users.noreply.github.com>
2022-12-09 19:54:59 -0500

    fixed memory issues

--- rum/src/rumdata.rs
+++ rum/src/rumdata.rs
@@ -54,13 +54,17 @@ impl RumData {
                 }
                 o if o == Opcode::SegLoad as u32 => {
                     let (reg_a, reg_b, reg_c) = get_three_regs(instruction);
-                    let value = self.memory.get_seg_val(reg_b as u32, reg_c);
+                    let segment = self.cpu.fetch_reg(reg_b) as usize;
+                    let offset = self.cpu.fetch_reg(reg_c) as usize;
+                    let value = self.memory.get_seg_val(segment, offset);
                     self.cpu.load_value(reg_a, value);
                 }
                 o if o == Opcode::SegStore as u32 => {
                     let (reg_a, reg_b, reg_c) = get_three_regs(instruction);
+                    let segment = self.cpu.fetch_reg(reg_a) as usize;
+                    let offset = self.cpu.fetch_reg(reg_b) as usize;
                     let value = self.cpu.fetch_reg(reg_c);
-                    self.memory.store_seg_val(reg_a as u32, reg_b, value);
+                    self.memory.store_seg_val(segment, offset, value);
                 }
                 o if o == Opcode::Add as u32 => {
                     let (reg_a, reg_b, reg_c) = get_three_regs(instruction);
@@ -83,11 +87,14 @@ impl RumData {
                 }
                 o if o == Opcode::Map as u32 => {
                     let (reg_b, reg_c) = get_two_regs(instruction);
-                    self.memory.map_seg(reg_b as u32, reg_c);
+                    let size = self.cpu.fetch_reg(reg_c) as usize;
+                    let ptr = self.memory.map_seg(size) as u32;
+                    self.cpu.load_value(reg_b, ptr);
                 }
                 o if o == Opcode::Unmap as u32 => {
                     let reg_c = get_one_reg(instruction);
-                    self.memory.unmap_seg(reg_c as u32);
+                    let ptr = self.cpu.fetch_reg(reg_c) as usize;
+                    self.memory.unmap_seg(ptr);
                 }
                 o if o == Opcode::Output as u32 => {
                     let reg_c = get_one_reg(instruction);
@@ -101,8 +108,10 @@ impl RumData {
                 }
                 o if o == Opcode::LoadProgram as u32 => {
                     let (reg_b, reg_c) = get_two_regs(instruction);
-                    self.memory.load_program(reg_b as u32, reg_c);
-                    self.cpu.pc = reg_c;
+                    let segment = self.cpu.fetch_reg(reg_b) as usize;
+                    let offset = self.cpu.fetch_reg(reg_c) as usize;
+                    self.memory.load_program(segment, offset);
+                    self.cpu.pc = offset;
                 }
                 o if o == Opcode::LoadValue as u32 => {
                     let load_reg = get_load_reg(instruction);
--- rum/src/rummemory.rs
+++ rum/src/rummemory.rs
@@ -1,10 +1,11 @@
-use std::collections::HashMap;
-
 /// A representation of the memory of a Universal Machine. Segment 0 (`seg0`) contains
 /// the current running program, while `active_segs` contains any other segments
 pub struct RumMemory {
-    pub seg0: Vec<u32>,
-    pub active_segs: HashMap<u32, Vec<u32>>,
+    // pub seg0: Vec<u32>,
+    // pub active_segs: HashMap<u32, Vec<u32>>,
+
+    pub segs: Vec<Vec<u32>>,
+    pub available_segs: Vec<u32>,
 }
 
 impl RumMemory {
@@ -16,7 +17,8 @@ impl RumMemory {
     /// # Arguments:
     /// * `program`: a `Vec<u32>` containing the instructions of the program given by the command line.
     pub fn init(program: Vec<u32>) -> Self {
-        RumMemory { seg0: program, active_segs: HashMap::new() }
+        //RumMemory { seg0: program, active_segs: HashMap::new() }
+        RumMemory { segs: vec![program], available_segs: Vec::new() }
     }
 
     /// Function to get a value in a particular segment at an offset. This function will
@@ -28,8 +30,9 @@ impl RumMemory {
     /// # Arguments:
     /// * `segnum`: The segment being looked for, representing a key in `active_segs`.
     /// * `offset`: The index of the value being extracted.
-    pub fn get_seg_val(&mut self, segnum: u32, offset: usize) -> u32 {
-        *self.get_seg(segnum).get(offset).unwrap()
+    pub fn get_seg_val(&mut self, segnum: usize, offset: usize) -> u32 {
+        //*self.get_seg(segnum).get(offset).unwrap()
+        *self.segs.get(segnum).unwrap().get(offset).unwrap()
     }
 
     /// Function to store a value in a particular segment at an offset. This function
@@ -42,8 +45,10 @@ impl RumMemory {
     /// * `segnum`: The segment being looked for, representing a key in `active_segs`.
     /// * `offset`: The index where the value will be inserted.
     /// * `value`: The value being stored into a segment.
-    pub fn store_seg_val(&mut self, segnum: u32, offset: usize, value: u32) {
-        let item = self.get_mut_seg(segnum).get_mut(offset).unwrap();
+    pub fn store_seg_val(&mut self, segnum: usize, offset: usize, value: u32) {
+        // let item = self.get_mut_seg(segnum).get_mut(offset).unwrap();
+
+        let item = self.segs.get_mut(segnum).unwrap().get_mut(offset).unwrap();
 
         *item = value;
     }
@@ -56,15 +61,14 @@ impl RumMemory {
     /// # Arguments:
     /// * `segnum`: The segment being mapped, representing a key that is not currently in `active_segs`.
     /// * `size`: The size of the segment that is being mapped.
-    pub fn map_seg(&mut self, segnum: u32, size: usize) {
-        if segnum == 0 {
-            panic!("Attempted to map $m[0], which already exists");
-        }
-
-        if !self.active_segs.contains_key(&segnum) {
-            self.active_segs.insert(segnum, vec![0; size]);
+    pub fn map_seg(&mut self, size: usize) -> usize {
+        if self.available_segs.is_empty() {
+            self.segs.push(vec![0; size]);
+            self.segs.len() - 1
         } else {
-            panic!("Attempted to map $m[{segnum}], which already exists");
+            let next_seg = self.available_segs.pop().unwrap() as usize;
+            self.segs[next_seg] = vec![0; size];
+            next_seg
         }
     }
 
@@ -76,15 +80,12 @@ impl RumMemory {
     /// 
     /// # Arguments:
     /// * `segnum`: The segment being unmapped, representing a key in `active_segs`.
-    pub fn unmap_seg(&mut self, segnum: u32) {
-        if segnum == 0 {
-            panic!("Attempted to unmap $m[0], which is illegal");
-        }
-
-        if self.active_segs.contains_key(&segnum) {
-            self.active_segs.remove(&segnum);
+    pub fn unmap_seg(&mut self, segnum: usize) {
+        if segnum >= self.segs.len() || self.available_segs.contains(&(segnum as u32)) {
+            panic!("Attempted to unmap $m[{segnum}], which is not active");
         } else {
-            panic!("Attempted to map $m[{segnum}], which does not exist");
+            self.segs[segnum].clear();
+            self.available_segs.push(segnum as u32);
         }
     }
 
@@ -99,43 +100,17 @@ impl RumMemory {
     /// # Arguments:
     /// * `segnum`: The segment being loaded into `seg0`, representing a key in `active_segs`.
     /// * `offset`: The offset that the program counter will be set to. This is used only to compare the new length of `seg0`
-    pub fn load_program(&mut self, segnum: u32, offset: usize) {
+    pub fn load_program(&mut self, segnum: usize, offset: usize) {
+        if segnum >= self.segs.len() || self.available_segs.contains(&(segnum as u32)) {
+            panic!("Attempted to load $m[{segnum}], which is not active");
+        }
+
         if segnum != 0 {
-            self.seg0 = self.get_seg(segnum).clone();
+            self.segs[0] = self.segs[segnum].clone();
         }
 
-        if offset >= self.seg0.len() {
-            panic!("Illegal access at $m[0][{offset}]");
-        }
-    }
-
-    /// Helper function for getting the appropriate segment based on the number provided.
-    /// This function will panic iff `segnum` points to a segment that is not active.
-    /// 
-    /// Returns a read-only reference to $m`[segnum]`
-    /// 
-    /// # Arguments:
-    /// * `segnum`: The segment being looked for, representing a key in `active_segs`
-    fn get_seg(&self, segnum: u32) -> &Vec<u32> {
-        if segnum == 0 {
-            &self.seg0
-        } else {
-            self.active_segs.get(&segnum).unwrap() //if .get returns None (segment is not there), unwrap() causes a panic!
-        }
-    }
-
-    /// Helper function for getting the appropriate segment based on the number provided.
-    /// This function will panic iff `segnum` points to a segment that is not active.
-    /// 
-    /// Returns a mutable reference to $m`[segnum]`
-    /// 
-    /// # Arguments:
-    /// * `segnum`: The segment being looked for, representing a key in `active_segs`
-    fn get_mut_seg(&mut self, segnum: u32) -> &mut Vec<u32> {
-        if segnum == 0 {
-            &mut self.seg0
-        } else {
-            self.active_segs.get_mut(&segnum).unwrap() //if .get_mut returns None (segment is not there), unwrap() causes a panic!
+        if offset >= self.segs[0].len() {
+            panic!("Illegal attempt to access memory at $m[{segnum}][{offset}]");
         }
     }
 }
